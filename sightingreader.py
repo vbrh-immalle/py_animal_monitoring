6f83bd441816e7d90ed1d0d882c6909ceaa8bfb4
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2020-01-30 14:29:25 +0100

    AnimalMonitor nog verder te implementeren

--- sightingreader.py
+++ sightingreader.py
@@ -1,4 +1,4 @@
-
+from dataclasses import dataclass
 
 @dataclass
 class Sighting:
@@ -11,14 +11,18 @@ class Sighting:
 
 class SightingReader:
 
-    def get_sightings(filename):
+    def get_sightings(self, filename):
         import csv
         sightings = []
-        # Parse CSV
-        # for row in csv:
-        #    s = Sighting(...)
-        # return sightings
-        pass
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                if len(row) != 5:
+                    print("Rij gevonden die geen 5 velden heeft... Ignoring...")
+                else:
+                    print(row)
+                    sightings.append(Sighting(row[0], row[1], row[2], row[3], row[4]))
+        return sightings
 
 
 class AnimalMonitor:
@@ -26,27 +30,34 @@ class AnimalMonitor:
     def __init__(self):
         self.sightings = []
 
-    def add_sightings(filename):
-        # vul self.sightings
+    def add_sightings(self, filename):
+        sr = SightingReader()
+        self.sightings = sr.get_sightings(filename)
+
+    def get_count(self, animal):
         pass
 
-    def get_count(animal):
+    def print_list(self):
+        for s in self.sightings:
+            print(s)
+
+    def print_sightings_by(self, spotter):
         pass
 
-    def print_list():
-        pass
-
-    def print_sightings_by(spotter):
-        pass
-
-    def print_sightings_of(animal):
+    def print_sightings_of(self, animal):
         pass
 
 
 if __name__ == "__main__":
-    animalMo1 = AnimalMonitor()
+    s = Sighting(0, "Olifant", 0, 0, 0)
+    print(s)
 
+    sr = SightingReader()
+    print(sr.get_sightings("sightings.csv"))
+
+    animalMo1 = AnimalMonitor()
     animalMo1.add_sightings("sightings.csv")
+    animalMo1.print_list()
     print(animalMo1.get_count("Elephant"))
     print(animalMo1.print_sightings_by(2)) # spotter nr 2?
     print(animalMo1.print_sightings_of("Elephant")) 
